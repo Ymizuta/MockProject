27f96c9ffd0197d8772f3109c82a6a0d5ef4c347
Ymizuta <suiden.53@gmail.com>
2018-10-27 15:37:42 +0900

    [add]モック2を追加

---
+++ Assets/script/mock2_sc/Move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    private Vector3 player_poz;
+    private float player_move_speed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical")) {
+            Debug.Log("移動中");
+            player_poz = gameObject.transform.position;
+            player_poz.x += Input.GetAxis("Horizontal") * player_move_speed;
+            player_poz.z += Input.GetAxis("Vertical") * player_move_speed;
+            gameObject.transform.position = player_poz;
+        }
+	}
+}
---
+++ Assets/script/mock2_sc/Shoot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+    [SerializeField] GameObject bullet = null;
+    private float shoot_speed = 1000.0f;
+    private float shoot_interval = 0f;
+    const float max_shoot_interval = 0.25f;
+    const float min_shoot_interval = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        shoot_interval -= Time.deltaTime;
+
+        if (Input.GetMouseButton(0))
+        {
+            if (shoot_interval <= min_shoot_interval ) {
+                GameObject new_bullet = Instantiate(bullet, gameObject.transform.position, gameObject.transform.rotation) as GameObject;
+                new_bullet.GetComponent<Rigidbody>().AddForce(transform.forward * shoot_speed);
+                Destroy(new_bullet, 3f);
+                shoot_interval = max_shoot_interval;
+            }
+        }
+	}
+}
